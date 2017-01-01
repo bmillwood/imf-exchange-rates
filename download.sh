8a6821b09331e3dc7c5fc6162b25d741e87b0233
Ben Millwood <thebenmachine+git@gmail.com>
2017-01-01 21:30:32 +0800

    10s excessive, we're not downloading loads of data

--- download.sh
+++ download.sh
@@ -3,7 +3,7 @@ yearmonths="$@"
 first=true
 for yearmonth in $yearmonths
 do
-	$first || sleep 10
+	$first || sleep 2
 	first=false
 	date=$(date --date="$yearmonth-01 +1 month -1 day" +%Y-%m-%d)
 	curl --fail "https://www.imf.org/external/np/fin/data/rms_mth.aspx?SelectDate=${date}&reportType=CVSDR&tsvflag=Y" \
