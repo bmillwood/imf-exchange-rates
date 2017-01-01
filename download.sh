07ba1cb5453f31cf5da5a679a8f2ff0ecf305cf0
Ben Millwood <thebenmachine+git@gmail.com>
2017-01-01 21:15:51 +0800

    better names for tsv files

--- download.sh
+++ download.sh
@@ -7,6 +7,6 @@ do
 	first=false
 	date=$(date --date="$yearmonth-01 +1 month -1 day" +%Y-%m-%d)
 	curl --fail "https://www.imf.org/external/np/fin/data/rms_mth.aspx?SelectDate=${date}&reportType=CVSDR&tsvflag=Y" \
-		--output $date.tsv \
+		--output $yearmonth.tsv \
 		|| break
 done
